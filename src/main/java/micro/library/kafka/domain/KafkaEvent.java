cc907dc7576adf4d3f7b222dc68a3649448a6ba5
Chris Turner <peavers@gmail.com>
2018-06-26 14:40:05 +1200

    * added lombok support * allow any type of object to be sent as the event. Up to the user to work it out.

--- src/main/java/micro/library/kafka/domain/KafkaEvent.java
+++ src/main/java/micro/library/kafka/domain/KafkaEvent.java
@@ -1,5 +1,16 @@
 package micro.library.kafka.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KafkaEvent {
-    String event;
+    Object event;
 }
